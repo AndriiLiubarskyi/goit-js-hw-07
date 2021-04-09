03a9ec9b4acc2ea3fe0a4e17a42b8c73f7ef8a03
AndriiLiubarskyi <heinzanlu@gmail.com>
2021-04-09 20:51:23 +0300

    hw-js-07ver03

--- js/task-05.js
+++ js/task-05.js
@@ -3,11 +3,12 @@ const refs = {
     output: document.querySelector('#name-output'),
 };
 
+
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    refs.output.textContent = `${event.currentTarget.value}`; 
+    if (event.currentTarget.value != "") {
+        refs.output.textContent = `${event.currentTarget.value}`;
+    } else { refs.output.textContent = `незнакомец` };
+    console.log(event.currentTarget.value);
 };
-
-
-
